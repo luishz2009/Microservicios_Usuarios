231c176e4b8909b5f5e228fab5b09060147f2ad9
luishz2009 <luishz2009@gmail.com>
2024-03-26 18:02:06 -0500

    Agregando el servidor eureka

--- usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
+++ usuario-service/src/main/java/com/usuario/service/UsuarioServiceApplication.java
@@ -2,10 +2,12 @@ package com.usuario.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients //Para comunicar los microservicios con FeignClient
 @SpringBootApplication
+@EnableEurekaClient
 public class UsuarioServiceApplication {
 
 	public static void main(String[] args) {
--- usuario-service/src/main/java/com/usuario/service/model/Carro.java
+++ usuario-service/src/main/java/com/usuario/service/model/Carro.java
@@ -1,8 +1,10 @@
 package com.usuario.service.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Carro {
 
     private String marca;
